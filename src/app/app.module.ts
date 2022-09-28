329d8133a9d35f60c3b56d87bd7f0d349a9dcbe2
HessahSWE <hissahalhussain@gmail.com>
2022-09-28 11:14:45 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule }   from '@angular/forms';
 import { HomeComponent } from './Components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +19,10 @@ import { HomeComponent } from './Components/home/home.component';
     BrowserModule,
     AppRoutingModule,
     GaugeModule.forRoot(),
-    FormsModule
+    FormsModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
