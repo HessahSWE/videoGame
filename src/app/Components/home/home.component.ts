a1c94fb2c22278ea01b7bf58a4a88f88299c2f40
HessahSWE <hissahalhussain@gmail.com>
2022-10-03 12:39:25 +0300

    update

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ActivatedRoute, Params } from '@angular/router';
+import { APIResponse, Game } from 'src/app/models';
+import { HttpService } from 'src/app/service/http.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-    sort?: string;
-  constructor() { }
+  sort?: string;
+  games?: Array<Game>;
+
+
+  constructor(
+    private httpService: HttpService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
+        this.searchGames('metacrit', params['game-search']);
+      }
+      else
+        this.searchGames('metacrit');
+    });
   }
-
+  searchGames(sort?: string, search?: string): void {
+    this.httpService.getGameList(sort, search).subscribe((
+      gameList: APIResponse<Game>) => {
+      this.games = gameList.results;
+      console.log(gameList);
+    });
+  }
 }
